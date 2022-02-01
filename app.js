3de7bd0635fa1bf9eb610e4e08669dd6628a4dc4
suares rocio <suarez.rocio4344@gmail.com>
2022-01-31 23:41:11 -0300

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ app.get('/', (req,res)=>{
   res.sendFile(__dirname + '/views/home.html')
 });
 
-app.listen (3000, ()=> {
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/login.html')
+});
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/register.html')
+});
+
+/*process.env.PORT */
+app.listen ( process.env.PORT || 3030 , ()=> {
 console.log('app corriendo en el puerto 3000')
 });
+
 
