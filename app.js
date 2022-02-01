f564fc1299fd6380a6244714972e28d722a91629
suares rocio <suarez.rocio4344@gmail.com>
2022-01-31 22:46:13 -0300

    add folders and files

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//import Express from ('/express')//
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+/*
+const path = path.require('path')
+*/
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/home.html')
+});
+
+app.listen (3000, ()=> {
+console.log('app corriendo en el puerto 3000')
+});
+
